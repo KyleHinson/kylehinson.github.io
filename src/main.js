c6e459b4bf5ee79ebb0e528c82582be41fece16c
Kaiyo <hinson0103@gmail.com>
2025-09-24 02:13:48 +0800

    Fixed import library issue by using raw git

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import 'phaser'
+/** @type {import("https://raw.githubusercontent.com/phaserjs/phaser/refs/heads/master/types/phaser.jsons")} */
+// For Github Page Build, Raw git file to import
 import './style.css'
 
 import {LoadScene} from "./scenes/LoadScene";
 import {MainMenu} from "./scenes/MainMenu";
 
-var w = window.screen.width;
+var w = window.screen.width +100;
 var h = window.screen.height; 
 
 let game = new Phaser.Game({
